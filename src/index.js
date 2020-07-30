4a1faaf08ac299c114e853ae73f38a2e9eb11f52
AlvaroMendes007 <alvaro-fontes@hotmail.com>
2020-07-29 23:05:35 -0300

    Add Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
+import Gif from '../src/assets/img/404.gif';
+// import styled from 'styled-components';
+
+// const Error = styled.error`
+//   text-align: 'center';
+// `;
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (
+  <div>
+    {/* <Error> */}
+      <img src={Gif} alt='404'/> 
+      <h1>Error 404</h1>
+    {/* </Error> */}
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
